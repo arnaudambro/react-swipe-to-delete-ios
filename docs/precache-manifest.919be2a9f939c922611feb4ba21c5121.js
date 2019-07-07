ee07f0f01085caf696ae125b900ba47ff7865356
arnaudambro <arnaud.ambroselli@yahoo.fr>
2019-07-07 14:08:44 +0200

    set proper docs file for demo

---
+++ docs/precache-manifest.919be2a9f939c922611feb4ba21c5121.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68171a38371cb926d8541868df05bd1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fddeed761beb65a4def1",
+    "url": "/static/js/2.7778f64c.chunk.js"
+  },
+  {
+    "revision": "38c534c5ee5a4896adcc",
+    "url": "/static/js/main.b084f2b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
