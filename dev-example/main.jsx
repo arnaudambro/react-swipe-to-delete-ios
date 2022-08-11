46126505cdf6dcf365ea0acac112e41789435935
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-08-11 12:02:43 +0200

    setup demo

---
+++ dev-example/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Demo from "./Demo";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Demo />
+  </React.StrictMode>
+);
